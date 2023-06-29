b709876b0dc40145f66ac695fbf8bb56d5a67e7a
NiazbekovKuralbek <n.k.a.k.28241916@gmail.com>
2023-06-30 00:57:20 +0600

    qwe

--- Controllers/PointSaleController.cs
+++ Controllers/PointSaleController.cs
@@ -15,7 +15,7 @@ using NPOI.Util;
 
 namespace AdminQRCodeMVC.Controllers
 {
-    //I was here)
+    //fsdfseefg
     [ApiController]
     [Route("api/pointsale")]
     public class PointSaleController : Controller
