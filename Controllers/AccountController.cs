bba9e6196b81e5b32e820dd8a408c8241ee6c11f
NiazbekovKuralbek <n.k.a.k.28241916@gmail.com>
2023-07-21 03:55:11 +0600

    Корректировка

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,75 +1,100 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using AdminQRCodeMVC.ViewModels;
+using Microsoft.AspNetCore.Authentication;
 
-namespace AdminQRCodeMVC.Controllers;
-public class AccountController : Controller
+namespace AdminQRCodeMVC.Controllers
 {
-    private readonly UserManager<IdentityUser> _userManager;
-    private readonly SignInManager<IdentityUser> _signInManager;
-
-    public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    public class AccountController : Controller
     {
-        _userManager = userManager;
-        _signInManager = signInManager;
-    }
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
-    [HttpGet]
-    public IActionResult Login()
-    {
-        var model = new LoginViewModel(); // Используйте вашу модель LoginViewModel
-        return View(model);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Login(LoginViewModel model)
-    {
-        if (ModelState.IsValid)
+        // Конструктор контроллера с внедрением UserManager и SignInManager через Dependency Injection
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
-            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
-            if (result.Succeeded)
-            {
-                return RedirectToAction("Index", "Home"); // Перенаправление на главную страницу после успешного входа
-            }
-            else
-            {
-                ModelState.AddModelError("", "Неверный email или пароль");
-            }
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        return View(model);
-    }
-
-    [HttpGet]
-    public IActionResult Register()
-    {
-        var model = new RegisterViewModel(); // Используйте вашу модель RegisterViewModel
-        return View(model);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Register(RegisterViewModel model)
-    {
-        if (ModelState.IsValid)
+        [HttpPost]   // .../Account/Logout
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
         {
-            var user = new IdentityUser { UserName = model.Email, Email = model.Email };
-            var result = await _userManager.CreateAsync(user, model.Password);
+            // Выход пользователя из системы с помощью метода SignOutAsync
+            await _signInManager.SignOutAsync();
 
-            if (result.Succeeded)
+            // Очистка сессии с помощью метода SignOutAsync из Microsoft.AspNetCore.Authentication
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction("Index", "Home"); // Перенаправление на главную страницу после выхода
+        }
+
+        
+        [HttpGet]   // .../Account/Login
+        public IActionResult Login()
+        {
+            var model = new LoginViewModel(); // Создание модели LoginViewModel для передачи в представление
+            return View(model); // Возврат представления, в котором будет отображаться форма для входа
+        }
+
+        
+        [HttpPost]   // .../Account/Login
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid) // Проверка, что данные в модели валидны (email и пароль введены корректно)
             {
-                await _signInManager.SignInAsync(user, false);
-                return RedirectToAction("Index", "Home"); // Перенаправление на главную страницу после успешной регистрации
-            }
-            else
-            {
-                foreach (var error in result.Errors)
+                // Попытка аутентификации пользователя с помощью метода PasswordSignInAsync
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+                if (result.Succeeded) // Если аутентификация успешна
                 {
-                    ModelState.AddModelError("", error.Description);
+                    return RedirectToAction("Index", "Home"); // Перенаправление на главную страницу после успешного входа
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неверный email или пароль"); // Ошибка аутентификации, добавляем ошибку в ModelState
                 }
             }
+
+            return View(model); // Возврат представления с моделью для отображения ошибок
         }
 
-        return View(model);
-    }
+        
+        [HttpGet]  // .../Account/Register
+        public IActionResult Register()
+        {
+            var model = new RegisterViewModel(); // Создание модели RegisterViewModel для передачи в представление
+            return View(model); // Возврат представления, в котором будет отображаться форма для регистрации
+        }
 
+        
+        [HttpPost]  // .../Account/Register
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid) // Проверка, что данные в модели валидны (email и пароль введены корректно)
+            {
+                // Создание нового пользователя с помощью класса IdentityUser и UserManager
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                // Попытка создания нового пользователя с помощью метода CreateAsync
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded) // Проверка создан ли пользователь 
+                {
+                    // Попытка аутентификации только что созданного пользователя с помощью метода SignInAsync
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home"); // Перенаправление на главную страницу после успешной регистрации
+                }
+                else
+                {
+                    // Если возникли ошибки при создании пользователя, добавляем их в ModelState
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(model); // Возврат представления с моделью для отображения ошибок
+        }
+    }
 }
